47b96af370e407e5aa785f14f0ac7d53d1deb504
Amrutha Chigilipilli <amrutha.c@zensar.com>
2024-05-30 09:21:12 +0530

    changes

--- src/main/java/com/zensar/sample_app_web/controller/MyController.java
+++ src/main/java/com/zensar/sample_app_web/controller/MyController.java
@@ -10,4 +10,13 @@ public class MyController {
 	public String sayHello() {
 		return "<h1>Welcome to Jenkins</h1>";
 	}
+	@RequestMapping("/hi")
+	public String sayHi() {
+		return "<h1>Welcome to Jenkins server</h1>";
+	}
+	@RequestMapping("/good")
+	public String sayZensar() {
+		return "<h1>Good Morning!!</h1>";
+	}
+	
 }
